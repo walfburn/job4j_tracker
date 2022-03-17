a152c1bc4d986f8059c341381359f46227fb5716
Vladimir <walfburner@mail.ru>
2022-03-18 00:12:17 +0300

    1.8. Состояние объекта [#167388 #276700]

--- src/main/java/ru/job4j/oop/Jar.java
+++ src/main/java/ru/job4j/oop/Jar.java
@@ -5,6 +5,18 @@ public class Jar {
 
     public Jar(int size) {
         this.value = size;
-        //123
+    }
+
+    public void pour(Jar another) {
+        this.value = this.value + another.value;
+        another.value = 0;
+    }
+
+    public static void main(String[] args) {
+        Jar first = new Jar(10);
+        Jar second = new Jar(5);
+        System.out.println(first.value + "??" + second.value);
+        first.pour(second);
+        System.out.println(first.value + "??" + second.value);
     }
 }
