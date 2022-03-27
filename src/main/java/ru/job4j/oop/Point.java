7e46c2918f86ea28f346be951fcd600a25554046
Vladimir <walfburner@mail.ru>
2022-03-27 19:45:34 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642]

---
+++ src/main/java/ru/job4j/oop/Point.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+public class Point {
+
+    private int x;
+
+    private int y;
+
+    public Point(int fisrt, int second) {
+        this.x = fisrt;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+}
---
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class TriangleTest {
+
+    @Test
+    public void when00and40and04Then8() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        assertThat(result, closeTo(8, 0.001));
+    }
+
+    @Test
+    public void testFail() {
+        Point a = new Point(0, 0);
+        Point b = new Point(1, 1);
+        Point c = new Point(2,2);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        assertThat(result, closeTo(4,0.001));
+    }
+}
